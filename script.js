a2e5b91dbed7598909dfedd38ee140b243c4c6fe
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-12-13 15:26:29 +0300

    Handle error incase it occurs

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ window.shimindexedDB;
 
 const request = indexDB.open('CarsDatabase', 1);
 
+request.onerror = function(event) {
+    console.log('An error occured in indexDB');
+    console.log(event)
+}
