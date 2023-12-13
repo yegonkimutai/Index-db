ca4fe566c88e61b3641b1306f3da8c59c7533708
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-12-13 15:44:43 +0300

    Handle successful requests and generate data

--- script.js
+++ script.js
@@ -14,7 +14,20 @@ request.onerror = function(event) {
 
 request.onupgradeneeded = function() {
     const db = request.result;
-    const store = db.createObjectStore('machines', { keyPath: "id"});
+    const store = db.createObjectStore('machines', { keyPath: "id" });
     store.createIndex('cars_color', ['color'], { unique: false });
     store.createIndex('color_and_make', ['color', 'make'], { unique: false })
+}
+
+request.onsuccess = function() {
+    const db = request.result;
+    const transaction = db.transaction('machines', 'readwrite');
+    const store = transaction.objectStore('machines');
+    const colorIndex = store.index('cars_color');
+    const makeIndex = store.index('color_and_make');
+
+    store.put({ id: 1, color: "Red", make: "Toyota"});
+    store.put({ id: 2, color: "Red", make: "KIA"});
+    store.put({ id: 3, color: "Blue", make: "Honda"});
+    store.put({ id: 4, color: "Silver", make: "Subaru"});
 }
