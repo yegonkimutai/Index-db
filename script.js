3fcff4ad3b3e4b767c72d4366a5d9a9ed3700cfb
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-12-13 15:56:05 +0300

    Handle successful messages

--- script.js
+++ script.js
@@ -26,8 +26,24 @@ request.onsuccess = function() {
     const colorIndex = store.index('cars_color');
     const makeIndex = store.index('color_and_make');
 
-    store.put({ id: 1, color: "Red", make: "Toyota"});
-    store.put({ id: 2, color: "Red", make: "KIA"});
-    store.put({ id: 3, color: "Blue", make: "Honda"});
-    store.put({ id: 4, color: "Silver", make: "Subaru"});
+    store.put({ id: 1, color: "Red", make: "Toyota" });
+    store.put({ id: 2, color: "Red", make: "KIA" });
+    store.put({ id: 3, color: "Blue", make: "Honda" });
+    store.put({ id: 4, color: "Silver", make: "Subaru" });
+
+    const idQuery = store.get(4);
+    const colorQuery = colorIndex.getAll(["Red"]);
+    const makeQuery = makeIndex.get(["Blue", "Honda"]);
+
+    idQuery.onsuccess = function() {
+        console.log('idQuery', idQuery.result);
+    }
+
+    colorQuery.onsuccess = function() {
+        console.log('colorQuery', colorQuery.result);
+    }
+
+    makeQuery.onsuccess = function() {
+        console.log('makeQuery', makeQuery.result);
+    }
 }
