005be8aadbcfecd76b442e49d1d4245c5c445b21
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-12-13 15:32:15 +0300

    Update database when necessary

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ request.onerror = function(event) {
     console.log('An error occured in indexDB');
     console.log(event)
 }
+
+request.onupgradeneeded = function() {
+    const db = request.result;
+    const store = db.createObjectStore('machines', { keyPath: "id"});
+    store.createIndex('cars_color', ['color'], { unique: false });
+    store.createIndex('color_and_make', ['color', 'make'], { unique: false })
+}
